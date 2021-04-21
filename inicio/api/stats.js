b86b98194e47e8a50a80acb79272599cb7c95472
CartzDEV <52172737+cartzdeveloper@users.noreply.github.com>
2021-04-21 01:19:32 -0300

    Delete inicio/api directory

--- inicio/api/stats.js
+++
@@ -1,9 +0,0 @@
-async function stats(request, response) {
-const statsResponse = await fetch(`https://canary.discord.com/api/guilds/812146641380573205/widget.json`);
-const statsResponseJson = statsResponse.json();
-const Guilds = statsResponseJson.members[9].game.name.replace('on' , '').replace('servers' , '')
-response.json({
-    guilds: Guilds,
-});
-}
-export default stats;
